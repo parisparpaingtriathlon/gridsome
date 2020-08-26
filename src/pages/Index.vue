eb826e271076f00eef3d5a1dfef050c665a1e30c
kinedie <cindy.liwenge@gmail.com>
2020-08-26 20:26:59 +0200

    :lipstick: add bouncing scrolldown button

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -18,7 +18,10 @@
         </div>
       </div>
     </section>
-    <section class="container news">
+    <section style="height: 10vh;">
+      <a href="#" class="scroll-down" id="scrollDown"></a>
+    </section>
+    <section class="container news" id="newsPreview">
       <h2>Précédemment, au PPT...</h2>
       <div class="row d-lg-flex justify-content-around">
         <div class="single-news col-sm-10 col-lg-6" v-for="{ node } in $page.allNewsItem.edges" :key="node._id">
@@ -126,7 +129,7 @@
 
   .jumbotron {
     background-image: url('../../placeholders/banner/simon-connellan-mfN2CYfjQz4-unsplash.jpg');
-    height: 50vh;
+    height: 46vh;
     color: #FE4365;
   }
 
@@ -228,4 +231,66 @@
   .social-ppt-legend:hover {
     transform: scale(1.2);
   }
+
+  /* Styling and animating the scroll-down button */
+  .scroll-down {
+    opacity: 1;
+    -webkit-transition: all .5s ease-in 3s;
+    transition: all .5s ease-in 3s;
+  }
+
+  .scroll-down {
+    position: absolute;
+    bottom: 30px;
+    left: 50%;
+    margin-left: -16px;
+    display: block;
+    width: 32px;
+    height: 32px;
+    border: 2px solid #FE4365;
+    background-size: 14px auto;
+    border-radius: 50%;
+    z-index: 2;
+    -webkit-animation: bounce 2s infinite 2s;
+    animation: bounce 2s infinite 2s;
+    -webkit-transition: all .2s ease-in;
+    transition: all .2s ease-in;
+    transform: scale(1);
+  }
+
+  .scroll-down:before {
+    position: absolute;
+    top: calc(50% - 8px);
+    left: calc(50% - 6px);
+    transform: rotate(-45deg);
+    display: block;
+    width: 12px;
+    height: 12px;
+    content: "";
+    border-width: 0px 0 2px 2px;
+    border-color: #FE4365;
+    border-style: solid;
+  }
+
+  @keyframes bounce {
+    0%,
+    100%,
+    20%,
+    50%,
+    80% {
+        -webkit-transform: translateY(0);
+        -ms-transform: translateY(0);
+        transform: translateY(0);
+    }
+    40% {
+        -webkit-transform: translateY(-10px);
+        -ms-transform: translateY(-10px);
+        transform: translateY(-10px);
+    }
+    60% {
+        -webkit-transform: translateY(-5px);
+        -ms-transform: translateY(-5px);
+        transform: translateY(-5px);
+    }
+}
 </style>
